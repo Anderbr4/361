d50e3cb413cbff5485ea8b7f3dfb1433c53d9dc1
Brian Anderson <anderbr4@engr.orst.edu>
2025-02-10 02:25:02 +0000

    work

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
 
 print("hello world")
+
+
+
+
+
+
+# create class todo item that stores to do item logic
+
+
+# store to do items in a list 
+
+# ui elements 
+
+
+
+# implement create to do item 
+
+# implement bulk delete to do items 
+
+# implement edit to do items 
+
+# implement view to do list 
+
+
+# 
