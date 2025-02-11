f560f14646bde7f327e036d98bb959715656a44d
Brian Anderson <anderbr4@engr.orst.edu>
2025-02-11 03:32:28 +0000

    work

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import random
 from pyfiglet import Figlet
 
-f = Figlet(font='slant')
-print(f.renderText('To Do Or Not To Do'))
 
 list_to_do_items = []
-
 
 # create class todo item that stores to do item logic
 
@@ -33,6 +30,7 @@ class ToDoItem:
             confirmation = input("Confirm these choices?: Y/N  ")
             print("\n")
             if confirmation == "Y":
+                print("\n You have modified the item!\n")
                 gathering = False
 
         self.item_name = new_item_name
@@ -57,16 +55,30 @@ def prompt_create_to_do_item():
         item_due_date = input("Enter Item Due Date: ")
 
         confirmation = input("Confirm these choices?: Y/N  ")
-        print("\n")
+
         if confirmation == "Y":
             gathering = False
 
     list_to_do_items.append(create_to_do_item(item_name,item_tag, item_comment, item_due_date))
-
+    print("\n You have created the item!\n")
 
 def view_all_items():
-    for item in list_to_do_items:
-        item.view_item_details()
+
+    user_view_choice = int(input("1. View all items\n2. View items by tag\nSelection: "))
+    if user_view_choice == 1:
+
+
+        print("\n Here are the remaining to do items: ")
+        for item in list_to_do_items:
+            item.view_item_details()
+    elif user_view_choice == 2:
+        user_tag = input("Enter tag: ")
+        print("\n Here are the remaining to do items: ")
+        for item in list_to_do_items:
+            if item.item_tag == user_tag:
+                item.view_item_details()
+    else:
+        print("input not recognized returning to main.\n")
 
 
 
@@ -77,35 +89,29 @@ def modify_to_do_item(item_id):
             item.modify_this_item()
 
 def delete_items_by_tag(tag):
-    for item in list_to_do_items:
-        if item.item_tag == tag:
-            list_to_do_items.remove(item)
+
+    delete_confirm= input("Are you sure you want to delete these items Y/N? Doing so is permanement: ")
+    if(delete_confirm == "Y"):
+        for item in list_to_do_items:
+            if item.item_tag == tag:
+                list_to_do_items.remove(item)
+        print("Deleted all items with tag: "+str(tag)+"\n")
+    else:
+        print("Canceled Delete. Returning to main menu\n")
 
 
 
 
-# print("Create To Do Item \nEnter Item Name:\nEnter Item Tag:\nEnter Item Comment:\nEnter Item Due Date:")
-
-# store to do items in a list 
-
-# ui elements 
+f = Figlet(font='slant')
+print(f.renderText('To Do Or Not To Do'))
 
 
 
-# implement create to do item 
-
-# implement bulk delete to do items 
-
-# implement edit to do items 
-
-# implement view to do list 
-
-
-# 
-
-
-
-
+list_to_do_items.append(create_to_do_item("test1",'test', 'just a test1', '02/10/25'))
+list_to_do_items.append(create_to_do_item("test2",'test', 'just a test2', '02/10/25'))
+list_to_do_items.append(create_to_do_item("test3",'test', 'just a test3', '02/10/25'))
+list_to_do_items.append(create_to_do_item("test4",'test', 'just a test4', '02/10/25'))
+list_to_do_items.append(create_to_do_item("Modify",'Modify', 'to be modified', '02/10/25'))
 
 
 using_tool = True
@@ -114,21 +120,19 @@ while(using_tool):
 
     
 # in the while loop 
+    print("Main Menu: ")
+
     print("1. View To Do List \n2. Add To Do Item \n3. (NEW) Modify To Do Item \n4. Delete To Do Item \n5. Exit\n")
     user_input = input("selection: ")
 
     if user_input == "1" :
-        print("viewing to do items: \n")
         view_all_items()
     elif user_input == "2":
-        print("adding to do items ")
         prompt_create_to_do_item()
     elif user_input == "3":
-        print("going to modify to do item")
         item_id = int(input("enter the id of the item to modify: "))
         modify_to_do_item(item_id)
     elif user_input == "4":
-        print("deleting to do item")
         tag = input("Please enter tag to delete: ")
         delete_items_by_tag(tag)
     elif user_input == "5":
@@ -136,21 +140,3 @@ while(using_tool):
         using_tool = False
     else:
         print("invalid selection please true again")
-
-
-# switch case 1
-#print("1. View All Items\n2. View Items By Tag\n3. View Items By Due Date")
-
-
-# switch case 2
-# print("Create To Do Item \nEnter Item Name:\nEnter Item Tag:\nEnter Item Comment:\nEnter Item Due Date:")
-
-
-# switch case 3
-# print("Not Done Yet? Modify!\nEnter Item Name:\nEnter Item Tag:\nEnter Item Comment:\nEnter Item Due Date:")
-
-
-#switch case 4 
-# print("Enter To Do List Item To Delete:\n\n1.Confirm Delete (Permanent) \n2. Exit")
-
-#switch case 5 exit 
